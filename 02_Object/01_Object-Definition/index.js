1c237025f9fcb1709d81f75073c29931bfcd2dc0
phuong2106 <phuong.nguyen@cel-consulting.com>
2019-07-23 13:12:41 +0700

    Object Definition

--- 02_Object/01_Object-Definition/index.js
+++ 02_Object/01_Object-Definition/index.js
@@ -28,5 +28,67 @@ const Login = () => {
 }
 
 const Contact = () => {
+    let doc = document.getElementById("root");
+    doc.innerHTML = ""
+
+    // Object admin
+    let admin = {
+        firstName: "Nguyen",
+        lastName: "Hoai Phuong",
+        age: 34,
+        city: "Ho Chi Minh",
+        country: "Vietnam"
+    };
+
+    let nameLabel = document.createElement("LABEL");
+    nameLabel.setAttribute("style", "width: 100px; display: inline-block")
+    nameLabel.innerHTML = "Name:"
+    doc.appendChild(nameLabel)
     
+    let nameTxt = document.createElement("INPUT")
+    nameTxt.setAttribute("TYPE", "text")
+    nameTxt.setAttribute("placeholder", admin.firstName + " " + admin.lastName)
+    doc.appendChild(nameTxt)
+
+    // New line
+    doc.appendChild(document.createElement("BR"))
+
+    let ageLabel = document.createElement("LABEL")
+    ageLabel.setAttribute("style", "width: 100px; display: inline-block")
+    ageLabel.innerHTML = "Age:"
+    doc.appendChild(ageLabel)
+
+    let ageTxt = document.createElement("INPUT")
+    ageTxt.setAttribute("TYPE", "text")
+    ageTxt.setAttribute("placeholder", admin.age.toString())
+    doc.appendChild(ageTxt)
+
+    // New line
+    doc.appendChild(document.createElement("BR"))
+
+    let cityLabel = document.createElement("LABEL")
+    cityLabel.setAttribute("style", "width: 100px; display: inline-block")
+    cityLabel.innerHTML = "City:"
+    doc.appendChild(cityLabel)    
+
+    let cityTxt = document.createElement("INPUT")
+    cityTxt.setAttribute("TYPE", "text")
+    cityTxt.setAttribute("placeholder", admin.city)
+    doc.appendChild(cityTxt)
+
+    // New line
+    doc.appendChild(document.createElement("BR"))
+
+    let countryLabel = document.createElement("LABEL")
+    countryLabel.setAttribute("style", "width: 100px; display: inline-block")
+    countryLabel.innerHTML = "Country:"
+    doc.appendChild(countryLabel)    
+
+    let countryTxt = document.createElement("INPUT")
+    countryTxt.setAttribute("TYPE", "text")
+    countryTxt.setAttribute("placeholder", admin.country)
+    doc.appendChild(countryTxt)
+
+    // New line
+    doc.appendChild(document.createElement("BR"))
 }
