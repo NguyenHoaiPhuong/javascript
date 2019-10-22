c85c8f1c695e98e0a3c3782f5100330a8e50eabe
phuong2106 <phuong.nguyen@cel-consulting.com>
2019-10-22 07:59:38 +0700

    AsyncCallback

---
+++ 00_Basic/12_Callback/Example_3/index.js
@@ -0,0 +1,16 @@
+function notifyAll(fnSms, fnEmail) {
+   setTimeout(function() {
+      console.log('starting notification process');
+      fnSms();
+      fnEmail();
+   }, 2000);
+}   
+
+notifyAll(function() {
+   console.log("Sms send ..");
+},
+function() {
+   console.log("email send ..");
+});
+
+console.log("End of script"); //executes first or not blocked by others  
