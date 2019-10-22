5b074c70a3b627c2af6e34ca03b4840912ba3a88
phuong2106 <phuong.nguyen@cel-consulting.com>
2019-10-22 09:21:59 +0700

    Promise - Catch, Final

--- 00_Basic/13_Promise/01_Basic/index.js
+++ 00_Basic/13_Promise/01_Basic/index.js
@@ -3,29 +3,29 @@ function getSum(n1, n2) {
     return n1 < 0 || n2 < 0;
   }
 
-  var promise = new Promise(function(resolve, reject) {
+  var promise = new Promise((resolve, reject) => {    
+    setTimeout(function() {      
+      resolve(n1 + n2);
+    }, 2000)
+
     if (isAnyNegative()) {
       reject(Error("Negatives not supported"));
     }
-    resolve(n1 + n2);
   })
 
   return promise;
 }
 
-getSum(5, 6)
+getSum(10, -5)
 .then(function(result) {
-  displayResult(result);
+  alert(result);
   return getSum(10, 20) // this returns another promise  
-}, displayError)
-.then(displayResult, displayError)
-
-function displayResult(result) {
-  console.log(result)
-}
-
-function displayError(error) {
-  console.log(error)
-}
+}, alert)
+.then(alert, alert)
+// .then(alert, null)
+.catch(alert)
+.finally(function() {
+  alert("promise is ready")
+})
 
 console.log("End of script ");
